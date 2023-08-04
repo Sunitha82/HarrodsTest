35ab4314f3d51e6269c7f1fe309eba645d11d3c9
Sunitha82 <sunitha.lakshman@gmail.com>
2023-08-04 19:24:09 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,161 @@
+﻿//using Pulumi;
+//using Pulumi.AzureNative.Resources;
+//using Pulumi.AzureNative.AppService;
+//using Pulumi.AzureNative.AppService.Inputs;
+//using Pulumi.AzureNative.KeyVault;
+//using Pulumi.AzureNative.KeyVault.Inputs;
+//using Pulumi.AzureNative.CosmosDB;
+//using Pulumi.AzureNative.CosmosDB.Inputs;
+//using Pulumi.AzureNative.ManagedServiceIdentity;
+//using Pulumi.AzureNative.ManagedServiceIdentity.Inputs;
+//using Pulumi.AzureNative.Network;
+//using Pulumi.AzureNative.Network.Inputs;
+//using System.Collections.Generic;
+
+//class Program
+//{
+//    static Task<int> Main(string[] args)
+//    {
+//        return Deployment.RunAsync(() =>
+//        {
+//            var resourceGroupName = "MyResourceGroup";
+//            var region = "UKSouth";
+
+//            // Create a resource group
+//            var resourceGroup = new ResourceGroup(resourceGroupName, new ResourceGroupArgs
+//            {
+//                Location = region
+//            });
+
+//            // Create a Windows-based Web App
+//            var appName = "MyWebApp";
+//            var appServicePlanName = "MyAppServicePlan";
+
+//            var appServicePlan = new AppServicePlan(appServicePlanName, new AppServicePlanArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                Location = region,
+//                Sku = new SkuDescriptionArgs
+//                {
+//                    Name = "B1",
+//                    Tier = "Basic",
+//                }
+//            });
+
+//            var webApp = new WebApp(appName, new WebAppArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                Location = region,
+//                ServerFarmId = appServicePlan.Id
+//            });
+
+//            // Create a Key Vault
+//            var keyVaultName = "MyKeyVault";
+//            var keyVault = new Vault(keyVaultName, new VaultArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                Location = region,
+//                Properties = new VaultPropertiesArgs
+//                {
+//                    TenantId = "[tenant-id]",
+//                    Sku = new SkuArgs
+//                    {
+//                        Family = "A",
+//                        Name = "standard"
+//                    },
+//                    AccessPolicies = new List<AccessPolicyEntryArgs>
+//                    {
+//                        new AccessPolicyEntryArgs
+//                        {
+//                            TenantId = "[tenant-id]",
+//                            ObjectId = webApp.Identity.PrincipalId,
+//                            Permissions = new PermissionsArgs
+//                            {
+//                                Secrets = new[] { "get", "list" }
+//                            }
+//                        }
+//                    }
+//                }
+//            });
+
+//            // Create Cosmos DB
+//            var cosmosDbName = "MyCosmosDB";
+//            var cosmosDb = new DatabaseAccount(cosmosDbName, new DatabaseAccountArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                Location = region,
+//                Kind = "GlobalDocumentDB",
+//                ConsistencyPolicy = new ConsistencyPolicyArgs
+//                {
+//                    DefaultConsistencyLevel = "Eventual",
+//                },
+//                Locations = new[]
+//                {
+//                    new LocationArgs
+//                    {
+//                        LocationName = region,
+//                        FailoverPriority = 0,
+//                    }
+//                },
+//            });
+
+//            // Create Managed Identity for Web App and grant permissions
+//            var identity = new ManagedServiceIdentity($"{webApp.Name}-identity", new ManagedServiceIdentityArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                Location = region,
+//                Tags = { { "type", "SystemAssigned" } },
+//            });
+
+//            var webAppPolicy = new AccessPolicy($"{webApp.Name}-policy", new AccessPolicyArgs
+//            {
+//                ResourceGroupName = resourceGroup.Name,
+//                VaultName = keyVault.Name,
+//                ObjectId = identity.PrincipalId,
+//                Permissions = new PermissionsArgs
+//                {
+//                    Secrets = new[] { "get", "list" }
+//                },
+//            });
+
+//            var cosmosDbPolicy = new DatabaseAccountSqlReadWritePolicy($"{cosmosDb.Name}-policy", new DatabaseAccountSqlReadWritePolicyArgs
+//            {
+//                AccountName = cosmosDb.Name,
+//                ResourceGroupName = resourceGroup.Name,
+//                Locations = new[] { new LocationArgs { LocationName = region } },
+//                SqlReadWriteMode = "ReadOnly",
+//                TenantId = "[tenant-id]",
+//                ObjectId = identity.PrincipalId,
+//            });
+
+//            // Restrict Network Connection
+//            var ipAddress = "192.168.1.1";
+//            var firewallRule = new FirewallRule($"{cosmosDb.Name}-firewallRule", new FirewallRuleArgs
+//            {
+//                AccountName = cosmosDb.Name,
+//                ResourceGroupName = resourceGroup.Name,
+//                IpAddressRange = ipAddress
+//            });
+
+//            // Setup Backup for Cosmos DB
+//            var backupPolicy = new DatabaseAccountBackupPolicy($"{cosmosDb.Name}-backupPolicy", new DatabaseAccountBackupPolicyArgs
+//            {
+//                AccountName = cosmosDb.Name,
+//                ResourceGroupName = resourceGroup.Name,
+//                PeriodicModeProperties = new PeriodicModePropertiesArgs
+//                {
+//                    PeriodicBackupProperties = new PeriodicBackupPropertiesArgs
+//                    {
+//                        BackupIntervalInMinutes = 60,
+//                        BackupRetentionIntervalInHours = 10
+//                    }
+//                }
+//            });
+
+//            // Create & Deploy a "Hello World" App
+//            // You need to deploy your app using your preferred deployment method (e.g., Pulumi deployments or other tools).
+
+//            return 0;
+//        });
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var resourceGroupName = "MyResourceGroup";
+    var region = "UKSouth";
+
+    // Create a resource group
+    var resourceGroup = new ResourceGroup(resourceGroupName, new ResourceGroupArgs
+    {
+        Location = region
+    });
+
+});
